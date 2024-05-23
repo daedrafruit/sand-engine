793bcbb9b12318f48cc49dd75949bf6204e664db
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-05-22 23:21:54 -0700

    started work on rendering pixels

--- falling-sand/RenderWindow.hpp
+++ falling-sand/RenderWindow.hpp
@@ -7,6 +7,9 @@ class RenderWindow {
 public:
 	RenderWindow(const char* p_title, int p_w, int p_h);
 	void cleanUp();
+	void clear();
+	void drawPixel(int p_x, int p_y, Uint8 p_r, Uint8 p_g, Uint8 p_b);
+	void display();
 
 private:
 	SDL_Window* window;
